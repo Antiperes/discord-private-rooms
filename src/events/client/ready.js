a00512eb64cb4b7244a8e3b905e847d0a23e384d
Antiperes <77325566+Antiperes@users.noreply.github.com>
2023-02-02 22:18:45 +0300

    Update ready.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -4,6 +4,6 @@ module.exports = {
     async execute(client) {
         require("../../handlers/commandHandler").init(client);
         require("../../mongo");
-        setInterval(() => { client.user.setPresence({ status: `dnd`, activities: [{ name: `Antiperes`, type: 2 }] }) }, 15 * 1000)
+        setInterval(() => { client.user.setPresence({ status: `dnd`, activities: [{ name: `Antipere$`, type: 2 }] }) }, 15 * 1000)
     }
 }
